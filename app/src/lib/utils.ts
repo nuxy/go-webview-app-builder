38a76a419accf51cb4644c3d66eed6ceaf528b73
Marc S. Brooks <marc@mbrooks.info>
2024-03-29 15:46:59 -0700

    Updated guard, check binding prefix
    Added JSDoc @example

--- app/src/lib/utils.ts
+++ app/src/lib/utils.ts
@@ -5,7 +5,11 @@
  *   Name of Go binding.
  *
  * @return {Boolean}
+ *
+ * @example
+ *   const result = webViewBindExists('browser_<Binding>');
+ *   // true
  */
 export function webViewBindExists(name: string | undefined): boolean {
-  return name && typeof window[name] === 'function';
+  return name && /^browser_/.test(name) && typeof window[name] === 'function';
 }
