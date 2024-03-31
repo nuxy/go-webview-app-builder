2c1bb499a8bf4626f0ddfe71a86095c305fbb438
Marc S. Brooks <marc@mbrooks.info>
2024-03-30 20:10:37 -0700

    Added Request/Response normalizer

--- app/src/lib/http.ts
+++ app/src/lib/http.ts
@@ -51,9 +51,9 @@ export class Request {
    *
    * @return {Promise<Response>}
    */
-  get(url: string): Promise<Response> {
+  async get(url: string): Promise<Response> {
     return (webViewBindExists('browser_HttpGet'))
-      ? window.browser_HttpGet(url)
+      ? this.normalize(await window.browser_HttpGet(url))
       : this.send('GET', url);
   }
 
@@ -68,9 +68,9 @@ export class Request {
    *
    * @return {Promise<Response>}
    */
-  head(url: string): Promise<Response> {
+  async head(url: string): Promise<Response> {
     return (webViewBindExists('browser_HttpHead'))
-      ? window.browser_HttpHead(url)
+      ? this.normalize(await window.browser_HttpHead(url))
       : this.send('HEAD', url);
   }
 
@@ -105,14 +105,14 @@ export class Request {
    *
    * @return {Promise<Response>}
    */
-  post(url: string, headers: RequestHeaders = {}, body?: RequestBody | string): Promise<Response> {
+  async post(url: string, headers: RequestHeaders = {}, body?: RequestBody | string): Promise<Response> {
     if (webViewBindExists('browser_HttpPost')) {
 
       // Convert data to Go bindings supported string types (e.g. JSON).
       const _headers = JSON.stringify(headers);
       const _body = (typeof body !== 'string') ? JSON.stringify(body) : body;
 
-      return window.browser_HttpPost(url, _headers, _body);
+      return this.normalize(await window.browser_HttpPost(url, _headers, _body));
     } else {
       return this.send('POST', url, headers, body);
     }
@@ -174,5 +174,17 @@ export class Request {
           Body: await response.text()
         };
       });
+  }
+
+  /**
+   * Ensure Go WebView bindings and development response match.
+   *
+   * @param {String} json
+   *   JSON string (Request encoded).
+   *
+   * @return {Response}
+   */
+  private normalize(json): Response {
+    return JSON.parse(json);
   }
 }
