6a5670bf7df64d95f9399633aa6454c60595dfe7
Marc S. Brooks <marc@mbrooks.info>
2024-04-03 16:08:49 -0700

    Support third-party CSS imports

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -41,6 +41,10 @@ module.exports = function(env, {analyze}) {
           exclude: /node_modules/
         },
         {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader']
+        },
+        {
           test: /\.scss$/i,
           use: ['style-loader', 'css-loader', 'sass-loader', {
             loader: 'postcss-loader',
