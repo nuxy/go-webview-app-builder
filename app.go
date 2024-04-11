e43d28620f6647b5fceb78766a6b5b0406583df2
Marc S. Brooks <marc@mbrooks.info>
2024-04-11 11:54:01 -0700

    Updated invalid (renamed) path

--- app.go
+++ app.go
@@ -42,7 +42,7 @@ func main() {
 	browser := lib.NewBrowser(htmlDoc, settings)
 
 	// Define browser WebView global bindings.
-	// @see app/src/lib/bindings.d.ts
+	// @see app/src/webview/bindings.d.ts
 	browser.BindFuncReturn("browser_AppVersion", func(_ ...string) string {
 		return Version
 	})
